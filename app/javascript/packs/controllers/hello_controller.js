7e06bd6495634629f13ad6e761c6ac212e5bd2e4
Tiago (pro2) <tiago.ameller@gmail.com>
2018-07-20 19:49:50 +0200

    make little changes to understand how stimulus controllers work

--- app/javascript/packs/controllers/hello_controller.js
+++ app/javascript/packs/controllers/hello_controller.js
@@ -2,12 +2,18 @@ import { Controller } from 'stimulus'
 
 export default class extends Controller {
   greet() {
-    console.log(`Hello, ${this.name}!`)
+    // console.log(`Hello, ${this.name}!`)
+    this.your_name.innerHTML = `Your name is: <strong>${this.name}</strong>`
   }
 
   get name() {
     return this.targets.find('name').value
   }
+
+  get your_name() {
+    return this.targets.find('your_name')
+  }
+
 }
 
 console.log('hello from hello controller')
